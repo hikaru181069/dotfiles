74b43fdf79f487ac29632089e4d62c63d7b91c40
Hikaru Sato <hikaru1069sato@icloud.com>
2026-04-26 23:10:01 +0900

    update nvim tmux wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,13 +1,18 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("Maple Mono NF")
+config.font = wezterm.font_with_fallback({
+	"Maple Mono NF",
+	"Maple Mono NF CN",
+	"Noto Color Emoji",
+})
+
 config.font_size = 21.0
 
 config.colors = {
-  cursor_bg = "#7f849c",     -- カーソル本体の色
-  cursor_fg = "#1e1e2e",     -- カーソル上の文字色
-  cursor_border = "#7f849c", -- 枠線 or Bar/Underline の色
+	cursor_bg = "#7f849c", -- カーソル本体の色
+	cursor_fg = "#1e1e2e", -- カーソル上の文字色
+	cursor_border = "#7f849c", -- 枠線 or Bar/Underline の色
 }
 
 config.color_scheme = "Catppuccin Mocha"
@@ -29,10 +34,10 @@ config.enable_scroll_bar = false
 
 -- パディング
 config.window_padding = {
-  left = 10,
-  right = 10,
-  top = 10,
-  bottom = 10,
+	left = 10,
+	right = 10,
+	top = 10,
+	bottom = 10,
 }
 
 return config
